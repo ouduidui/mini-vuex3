1d1d8ec82dd55ce614ee3711fb847dfcb4cc841c
oushihao <oushihao97@163.com>
2021-12-19 14:38:08 +0800

    style: 修改项目结构

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <div @click="$store.commit('add')">
+            {{$store.state.counter}}
+        </div>
+        <div @click="$store.dispatch('add')">
+            {{$store.getters.doubleCount}}
+        </div>
+    </div>
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from '../../lib/vuex'
+
+// this.$store
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        counter: 0
+    },
+    mutations: {
+        add(state) {
+            state.counter++;
+        }
+    },
+    actions: {
+        add({commit}){
+            setTimeout(() => {
+                commit('add');
+            },1000)
+        }
+    },
+    getters: {
+        doubleCount(state) {
+            return state.counter * 2;
+        }
+    },
+    modules: {}
+})
